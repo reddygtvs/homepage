d96fa0b41588b321050a420755c100d0e64b21da
Tushar <reddygtvs@gmail.com>
2022-11-04 05:25:58 +0530

    initial boilerplate, icon, centered div

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div id="control" className="App">
+      <div className="App-header">
+        <h1>Hi, I'm Tushar :)</h1>
+        <a
+          className="App-link"
+          href="https://github.com/reddygtvs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ./Github
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default App;
