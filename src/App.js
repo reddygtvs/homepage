a14481b1a0752af2a9f7cde1f9bf90a475d35676
Tushar <reddygtvs@gmail.com>
2022-11-05 03:09:07 +0530

    changed links, added styling transitions

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ const App = () => {
     <div id="control" className="App">
       <div className="App-header">
         <h1>Hi, I'm Tushar :)</h1>
+      </div>
+      <div className="Horizon">
         <a
           className="App-link"
           href="https://github.com/reddygtvs"
@@ -12,6 +14,41 @@ const App = () => {
           rel="noopener noreferrer"
         >
           ./Github
+        </a>
+
+        <a
+          className="App-link"
+          href="https://phonebook.tusharreddy.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Phonebook
+        </a>
+
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/g-tushar-reddy-551018207/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+        <a
+          className="App-link"
+          href="https://twitter.com/reddygtvs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          @Twitter
+        </a>
+
+        <a
+          className="App-link"
+          href="mailto:name@email.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contact Me
         </a>
       </div>
     </div>
