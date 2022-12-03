36e617d1df8dd5a7a11eeece5c68ea6ea2368c8e
Tushar <reddygtvs@gmail.com>
2022-12-03 21:21:01 +0530

    added navbar template

--- src/App.js
+++ src/App.js
@@ -1,60 +1,69 @@
 import "./App.css";
 import pdf from "./pdf/tushar-resume.pdf";
+import bootstrap from "bootstrap";
+import BasicExample from "./BasicExample";
+import Button from "react-bootstrap/Button";
 
 const App = () => {
   return (
-    <div id="control" className="App">
-      <div className="horizon">
-        <h1>
-          Hi, I'm Tushar <span class="dot">:)</span>
-        </h1>
+    <div>
+      <div id="navbar">
+        <BasicExample />
       </div>
-      <div>
-        <h3>Git | React | Node.js | Express | Cloudflare | Fly.io</h3>
-      </div>
-      <div className="Horizon">
-        <a
-          className="App-link"
-          href="https://github.com/reddygtvs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          ./Github
-        </a>
 
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/g-tushar-reddy-551018207/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
-        <a
-          className="App-link"
-          href="https://twitter.com/reddygtvs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          @Twitter
-        </a>
-        <a
-          className="App-link"
-          href={pdf}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Resume
-        </a>
+      <div id="control" className="App">
+        <div className="horizon">
+          <h1>
+            Hi, I'm Tushar <span class="dot">:)</span>
+          </h1>
+        </div>
+        <div>
+          <h3>Git | React | Node.js | Express | Cloudflare | Fly.io</h3>
+        </div>
+        <div className="Horizon">
+          <a
+            className="App-link"
+            href="https://github.com/reddygtvs"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            ./Github
+          </a>
 
-        <a
-          className="App-link"
-          href="mailto:reddygtvs@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Contact Me
-        </a>
+          <a
+            className="App-link"
+            href="https://www.linkedin.com/in/g-tushar-reddy-551018207/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+          <a
+            className="App-link"
+            href="https://twitter.com/reddygtvs"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @Twitter
+          </a>
+          <a
+            className="App-link"
+            href={pdf}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Resume
+          </a>
+
+          <a
+            className="App-link"
+            href="mailto:reddygtvs@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Contact Me
+          </a>
+        </div>
       </div>
     </div>
   );
