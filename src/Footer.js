b4e7790acf9b469515d8fcc9972084c00d3bb9e7
Tushar <reddygtvs@gmail.com>
2023-04-15 00:38:47 +0530

    changed link to open in new tab

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,12 @@
 const Footer = () => {
   return (
     <footer className="bottom">
-      <a id="footer" href="https://github.com/reddygtvs/homepage">
+      <a
+        id="footer"
+        href="https://github.com/reddygtvs/homepage"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         @Source | 2022 - 2023
       </a>
     </footer>
