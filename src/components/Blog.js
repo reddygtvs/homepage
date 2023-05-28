a53a8137adce836283b5fa491383cf932132021d
Tushar <reddygtvs@gmail.com>
2023-05-28 21:15:08 +0530

    changed padding to prevent text getting too close to border on mobile

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,6 +1,6 @@
 const Blog = () => {
   return (
-    <div>
+    <div className="Blog-home">
       <h1 className="name">Blog</h1>
       <p>Coming soon, working on setting up routing & MDX...</p>
     </div>
