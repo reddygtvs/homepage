ba794e2655029cffc153a662d60500d8b87bcabb
Tushar <reddygtvs@gmail.com>
2023-05-31 11:39:04 +0530

    changed blog title/text from mdx

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,9 +2,9 @@ import MdxComponent from "./MdxComponent";
 const Blog = () => {
   return (
     <div className="Blog-home">
-      {/* <h1 className="name">Blog</h1>
-      <p>Coming soon, working on setting up routing & MDX...</p> */}
-      <MdxComponent />
+      <h1 className="name">Blog</h1>
+      <p id="black">Coming soon, working on setting up routing & MDX...</p>
+      {/* <MdxComponent /> */}
     </div>
   );
 };
