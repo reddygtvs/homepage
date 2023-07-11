7128a21f1184b8b60af6942f71b2e26a121093f6
Tushar <reddygtvs@gmail.com>
2023-07-11 12:21:48 +0530

    enabled lazy loading for images

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ const App: React.FC = () => {
             roundedCircle={true}
             src={pfp}
             alt="pfp"
+            loading="lazy"
             style={{ maxWidth: "250px", maxHeight: "250px" }}
           />
         </div>
