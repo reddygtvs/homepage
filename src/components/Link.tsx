307fe0da8ed06287f17c1b6eacc0d31ece3c5be1
Tushar <reddygtvs@gmail.com>
2023-07-17 23:20:57 +0530

    added icons and edited styling for links

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -3,16 +3,19 @@ import React from "react";
 interface LinkProps {
   href: string;
   name: string;
+  imageSrc: string;
+  imageAlt: string;
 }
 
-const Link: React.FC<LinkProps> = ({ href, name }) => {
+const Link: React.FC<LinkProps> = ({ href, name, imageSrc, imageAlt }) => {
   return (
     <a
-      className="App-link underline-offset-2 mt-[15px] rounded-[0.5vh] inline-flex mx-[1.5vw] px-[0.5vw]"
+      className="App-link underline-offset-4 decoration-2 decoration-red-600 mt-[15px] rounded-lg inline-flex mx-[1.5vw] px-2 py-1 text-black hover:text-white hover:bg-green-300 hover:underline-offset-2 "
       href={href}
       target="_blank"
       rel="noopener noreferrer"
     >
+      <img src={imageSrc} alt={imageAlt} className="image-class" />
       {name}
     </a>
   );
--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -1,28 +1,42 @@
 import React from "react";
 import Link from "./Link";
 import pdf from "../pdf/tushar-resume.pdf";
+import githubIcon from "../icons/github2.svg";
+import linkedinIcon from "../icons/linkedin.svg";
+import resumeIcon from "../icons/resume.svg";
+import contactIcon from "../icons/mail.svg";
 
 interface TagData {
   href: string;
   name: string;
+  imageSrc: string;
+  imageAlt: string;
 }
 
 const Tags: React.FC = () => {
   const github: TagData = {
     href: "https://github.com/reddygtvs",
-    name: "./Github",
+    name: "Github",
+    imageSrc: githubIcon,
+    imageAlt: "Github",
   };
   const linkedin: TagData = {
     href: "https://www.linkedin.com/in/tushar-reddy/",
-    name: "@LinkedIn",
+    name: "LinkedIn",
+    imageSrc: linkedinIcon,
+    imageAlt: "LinkedIn",
   };
   const resume: TagData = {
     href: pdf,
     name: "Resume",
+    imageSrc: resumeIcon,
+    imageAlt: "Resume",
   };
   const contact: TagData = {
     href: "mailto:tusharreddy2023@gmail.com",
-    name: "Contact Me",
+    name: "Contact",
+    imageSrc: contactIcon,
+    imageAlt: "Contact Me",
   };
 
   return (
