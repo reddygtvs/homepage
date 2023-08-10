a0c38c8a0455f1fa72b5952534355b17f0c8763f
Tushar <reddygtvs@gmail.com>
2023-08-10 05:03:33 -0700

    removed redundant div

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -32,31 +32,29 @@ const Blog: React.FC = () => {
       <h1 className=" text-4xl py-4 underline mb-3 text-purple-700 decoration-4 max-w-[700px] decoration-cyan-500 font-bold p-2 text-left">
         A Collection of my programming thoughts
       </h1>
-      <div>
-        <ul>
-          {blogsData.map((blog) => (
-            <li key={blog.filename}>
-              <Link to={`/blogs/${blog.filename}`}>
-                <div className="card inline-flex max-[580px]:max-w-[360px] w-[800px] my-3 bg-slate-100 rounded-md overflow-hidden shadow-lg ">
-                  <div className="pl-4 pr-2 pt-4 pb-2">
-                    <div className="font-bold text-2xl mb-2 text-fuchsia-600 underline decoration-pink-600 decoration-2">
-                      {blog.title}
-                    </div>
-                    <p className="text-grey-darker text-base  font-thin">
-                      {blog.description}
-                    </p>
+      <ul>
+        {blogsData.map((blog) => (
+          <li key={blog.filename}>
+            <Link to={`/blogs/${blog.filename}`}>
+              <div className="card inline-flex max-[580px]:max-w-[360px] w-[800px] my-3 bg-slate-100 rounded-md overflow-hidden shadow-lg ">
+                <div className="pl-4 pr-2 pt-4 pb-2">
+                  <div className="font-bold text-2xl mb-2 text-fuchsia-600 underline decoration-pink-600 decoration-2">
+                    {blog.title}
                   </div>
-                  <div className="px-4 pb-2 text-right">
-                    <span className="italic underline decoration-2 decoration-emerald-500  inline-block bg-grey-lighter rounded-full text py-1 text-sm font-semibold text-grey-darker mr-2  text-emerald-500">
-                      {blog.date}
-                    </span>
-                  </div>
+                  <p className="text-grey-darker text-base  font-thin">
+                    {blog.description}
+                  </p>
                 </div>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+                <div className="px-4 pb-2 text-right">
+                  <span className="italic underline decoration-2 decoration-emerald-500  inline-block bg-grey-lighter rounded-full text py-1 text-sm font-semibold text-grey-darker mr-2  text-emerald-500">
+                    {blog.date}
+                  </span>
+                </div>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
