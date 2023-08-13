bc15781aca6cfb8109922efad7929c6529646629
Tushar <reddygtvs@gmail.com>
2023-08-13 00:21:01 -0700

    removed comments from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,6 @@ import { defineConfig, UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
-// import mdx from "@mdx-js/rollup";
-
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react(), viteTsconfigPaths(), svgrPlugin(), mdx()],
-// });
 
 export default defineConfig(async () => {
   const mdx = await import("@mdx-js/rollup");
